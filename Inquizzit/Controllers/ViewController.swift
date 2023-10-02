b3bb5c1af23096b4b44fed7291a5da86da1a7238
anishapareek <pareekanisha26@gmail.com>
2023-10-02 17:40:04 +0530

    - implement the protocol delegate pattern between quiz model and view controller to pass the fetched questions - add local question data json file

--- Inquizzit/Controllers/ViewController.swift
+++ Inquizzit/Controllers/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, QuizProtocol {
+    
+    var model = QuizModel()
+    var questions = [Question]()
+    var currentQuestionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        model.delegate = self
+        
+        model.getQuestions()
     }
-
-
+    
+    // MARK: - Quiz Protocol Methods
+    func questionsRetrieved(_ questions: [Question]) {
+        print("Questions retrieved from model!")
+    }
 }
 
--- Inquizzit/Models/QuizModel.swift
+++ Inquizzit/Models/QuizModel.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 
+protocol QuizProtocol {
+    func questionsRetrieved(_ questions: [Question])
+}
+
 class QuizModel {
+    
+    var delegate: QuizProtocol?
     
     func getQuestions() {
         
+        // TODO: Fetch the questions
+        
+        // Notify the delegate of the retrieved questions
+        delegate?.questionsRetrieved([Question]())
     }
+
 }
