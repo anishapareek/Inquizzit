91fc6dc59d0196dda2af53c0c5b944a7716a9678
anishapareek <pareekanisha26@gmail.com>
2023-10-10 19:18:05 +0530

    - add state manager class to save user state in user defaults

---
+++ Inquizzit/Controllers/StateManager.swift
@@ -0,0 +1,42 @@
+//
+//  StateManager.swift
+//  Inquizzit
+//
+//  Created by Anisha Pareek on 10/10/23.
+//
+
+import Foundation
+
+class StateManager {
+    
+    static var numCorrectKey = "NumberCorrectKey"
+    static var questionIndexKey = "QuestionIndexKey"
+    
+    static func saveState(numCorrect: Int, questionIndex: Int) {
+        
+        // Get a reference to user defaults
+        let defaults = UserDefaults.standard
+        
+        // Save the state data
+        defaults.set(numCorrect, forKey: numCorrectKey)
+        defaults.set(questionIndex, forKey: questionIndexKey)
+    }
+    
+    static func retrieveValue(key: String) -> Any? {
+        
+        // Get a reference to user defaults
+        let defaults = UserDefaults.standard
+        
+        return defaults.value(forKey: key)
+    }
+    
+    static func clearState() {
+        
+        // Get a reference to user defaults
+        let defaults = UserDefaults.standard
+        
+        // Clear the state data in user defaults
+        defaults.removeObject(forKey: numCorrectKey)
+        defaults.removeObject(forKey: questionIndexKey)
+    }
+}
